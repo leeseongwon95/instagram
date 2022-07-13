d9ff95e8b1d2cd8d4ede9704bf1c738304f83baa
seongwon <syanggu@naver.com>
2022-07-13 21:06:42 +0900

    theme.of and navigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
+import './style.dart' as style; // import 할 때 변수 중복문제 피하기
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        iconTheme: IconThemeData(color: Colors.blue),
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          actionsIconTheme: IconThemeData(color: Colors.black)
-        ),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.red),
-        )
-      ), // css style 태그 같은 느낌 ThemeData 위젯은 가까운 스타일을 가장먼저 적용함
+      theme: style.theme,
       home: MyApp(), // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
     )
   );
@@ -25,11 +17,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Instagram', style: TextStyle(color: Colors.black),),
+        title: Text('Instagram'),
         actions: [
-          Icon(Icons.add_box_outlined)
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.add_box_outlined),
+            iconSize: 30,
+          ),
       ],),
-      body: Text(' ')
-    );
+      body: TextButton(onPressed: (){}, child: Text('btn', style: Theme.of(context).textTheme.bodyText2),),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
+      ],
+      ),
+    ); // Theme.of 로 원하는 ThemeData 안의 내용 쓸수있음
   }
 }
