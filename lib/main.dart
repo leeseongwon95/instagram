7c5476f58dbcd609726ea0d79af19a324d0b589c
seongwon <syanggu@naver.com>
2022-07-14 21:55:00 +0900

    getData, json Handling2

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,11 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: style.theme,
-      home: MyApp(), // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: style.theme,
+    home: MyApp(), // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -22,11 +20,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
+  var data = [];
 
   getData() async {
-    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
     var result2 = jsonDecode(result.body);
-    print(result2);
+    setState(() {
+      data = result2;
+    });
     // http.get 함수도 오래걸리는 함수임 (전문용어로 Future)
   }
 
@@ -36,7 +38,6 @@ class _MyAppState extends State<MyApp> {
     getData();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,46 +45,55 @@ class _MyAppState extends State<MyApp> {
         title: Text('Instagram'),
         actions: [
           IconButton(
-            onPressed: (){},
+            onPressed: () {},
             icon: Icon(Icons.add_box_outlined),
             iconSize: 30,
           ),
-      ],),
-      body: [Home(), Text('샵')][tab],
+        ],
+      ),
+      body: [Home(data: data), Text('샵')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap: (i){
-          setState((){
+        onTap: (i) {
+          setState(() {
             // onTap 으로 i 가 바뀜 (네비게이션바의 숫자가)
             tab = i;
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
-      ],
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
+        ],
       ),
     ); // Theme.of 로 원하는 ThemeData 안의 내용 쓸수있음
   }
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({Key? key, this.data}) : super(key: key);
+  final data;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount:3, itemBuilder:(c, i){
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network('https://codingapple1.github.io/kona.jpg'),
-          Text('좋아요 100'),
-          Text('글쓴이'),
-          Text('글내용'),
-        ],
-
-      );
-    });
+    if (data.isNotEmpty) {
+      return ListView.builder(
+          itemCount: 3,
+          itemBuilder: (c, i) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(data[i]['image']),
+                Text(data[i]['likes'].toString()),
+                Text(data[i]['user']),
+                Text(data[i]['content']),
+              ],
+            );
+          });
+    } else {
+      return Text('로딩중임');
+    }
   }
 }
