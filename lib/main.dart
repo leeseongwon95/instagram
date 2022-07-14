7b90b885ddd84f1c889f3b71c55176da6b6b2d39
seongwon <syanggu@naver.com>
2022-07-14 21:03:44 +0900

    onTab, state UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,22 @@ import './style.dart' as style; // import 할 때 변수 중복문제 피하기
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: style.theme,
       home: MyApp(), // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
     )
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +33,16 @@ class MyApp extends StatelessWidget {
             iconSize: 30,
           ),
       ],),
-      body: TextButton(onPressed: (){}, child: Text('btn', style: Theme.of(context).textTheme.bodyText2),),
+      body: [Text('홈'), Text('샵')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        onTap: (i){
+          setState((){
+            // onTap 으로 i 가 바뀜 (네비게이션바의 숫자가)
+            tab = i;
+          });
+        },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'home'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
--- lib/style.dart
+++ lib/style.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 var theme = ThemeData(
-    textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-            backgroundColor: Colors.grey,
-        )
-    ),
-    appBarTheme: AppBarTheme(
-        color: Colors.white,
-        elevation: 1, // 그림자
-        titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
-        actionsIconTheme: IconThemeData(color: Colors.black)
-    ),
-    textTheme: TextTheme(
-    bodyText2: TextStyle(color: Colors.red),
-        // css style 태그 같은 느낌 ThemeData 위젯은 가까운 스타일을 가장먼저 적용함
-) // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.black
+  ),
+  textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.grey,
+      )
+  ),
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    elevation: 1, // 그림자
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+    actionsIconTheme: IconThemeData(color: Colors.black),
+  ),
+  textTheme: TextTheme(
+    bodyText2: TextStyle(color: Colors.red), // css style 태그 같은 느낌 ThemeData 위젯은 가까운 스타일을 가장먼저 적용함
+), // 복잡한 위젯은 복잡한위젯Theme() 안에서 스타일 줘야함
 );
